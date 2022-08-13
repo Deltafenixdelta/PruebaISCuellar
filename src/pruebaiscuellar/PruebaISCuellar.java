37c6e35b6391a720a0f1c53af85add85bb84dbfe
SPARTAN PC <SPARTAN PC@DESKTOP-LE49U0J>
2022-08-13 09:27:39 -0500

    Prueba IS Cuellar

--- src/pruebaiscuellar/PruebaISCuellar.java
+++ src/pruebaiscuellar/PruebaISCuellar.java
@@ -9,6 +9,7 @@ public class PruebaISCuellar {
      */
     public static void main(String[] args) {
         System.out.println("Prueba GITHUB TOVAR");
+        System.out.println("Hola mundo");
     }
     
 }
